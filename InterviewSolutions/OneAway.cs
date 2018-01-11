10e4ea50b427532688151c4a1b95fdd91c442b55
Venkadesan Vanchimuthu <vshyam.muthu@hotmail.com>
2018-01-10 23:17:37 -0700

    committing changes

---
+++ InterviewSolutions/OneAway.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewSolutions
+{
+    class OneAway
+    {
+        public Boolean oneEditAway(string first, string second)
+        {
+            if (first.Length == second.Length)
+                return oneEditReplace(first, second);
+            else if (first.Length + 1 == second.Length)
+                return oneEditInsert(first, second);
+            else if (first.Length - 1 == second.Length)
+                return oneEditInsert(second, first);
+            return false;
+
+        }
+
+        private Boolean oneEditReplace(string s1, string s2)
+        {
+            Boolean foundDifference = false;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    if (foundDifference) return false;
+                    foundDifference = true;
+                }
+            }
+            return true;
+        }
+
+        private Boolean oneEditInsert(string s1, string s2)
+        {
+            int index1 = 0;
+            int index2 = 0;
+            while (index2 < s2.Length && index1 < s1.Length)
+            {
+                if (s1[index1] != s2[index2])
+                {
+                    if (index1 != index2)
+                    {
+                        return false;
+                    }
+                    index2++;
+                }
+                else {
+                    index1++;
+                    index2++;
+                }
+            }
+            return true;
+        }
+
+
+        public Boolean oneEditAwayVersion1(string first, string second)
+        {
+            if ((first.Length - second.Length) > 1) return false;
+
+            string s1 = first.Length < second.Length ? first : second;
+            string s2 = first.Length < second.Length ? second : first;
+
+            int index1 = 0;
+            int index2 = 0;
+            Boolean foundDifference = false;
+            while (index2 < s2.Length && index1 < s1.Length)
+            {
+                if (s1[index1] != s2[index2])
+                {
+                    if (foundDifference) return false;
+                    foundDifference = true;
+                    if (s1.Length == s2.Length) index1++;
+
+                }
+                else {
+                    index1++;
+                }
+                index2++;
+            }
+            return true;
+        }
+    }
+}
--- InterviewSolutions/Program.cs
+++ InterviewSolutions/Program.cs
@@ -10,17 +10,31 @@ namespace InterviewSolutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Is a Palindrome"); 
-            Palindrome pal = new Palindrome();
-            string palindromCharacter = Console.ReadLine();
-            List<string> lstOfPermutations = new List<string>();
-            lstOfPermutations.Add(palindromCharacter);
-            pal.isPermuationPalindrome(palindromCharacter); 
-            if (pal.isPermuationPalindrome(palindromCharacter))
+            //Console.WriteLine("Is a Palindrome"); 
+            //Palindrome pal = new Palindrome();
+            //string palindromCharacter = Console.ReadLine();
+            //List<string> lstOfPermutations = new List<string>();
+            //lstOfPermutations.Add(palindromCharacter);
+            //pal.isPermuationPalindrome(palindromCharacter); 
+            //if (pal.isPermuationPalindrome(palindromCharacter))
+            //{
+            //    Console.WriteLine(String.Format("The input character is a Palindrome {0}", palindromCharacter));
+            //    Console.Read();  
+            //}
+
+            OneAway oAway = new OneAway();
+            //if (oAway.oneEditAway("pale", "pae"))
+            //    Console.Write("Both Strings are one edit away");
+            //else
+            //    Console.Write("Both Strings are Not one Edit Away");
+
+            if (oAway.oneEditAwayVersion1("pale", "ble"))
             {
-                Console.WriteLine(String.Format("The input character is a Palindrome {0}", palindromCharacter));
-                Console.Read();  
+                Console.Write("Both Strings are one edit away");
             }
+            else
+                Console.Write("Both Strings are Not one Edit Away");
+
         }
     }
 }
