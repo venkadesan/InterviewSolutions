1ac3bb48367cfc5dbd628409ac0ffbcb03199fe0
Venkadesan Vanchimuthu <vshyam.muthu@hotmail.com>
2018-07-01 12:37:01 -0600

    Binary Search Tree Data Structure

--- InterviewSolutions/Program.cs
+++ InterviewSolutions/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InterviewSolutions.Tree;
+
 
 namespace InterviewSolutions
 {
@@ -10,6 +12,8 @@ namespace InterviewSolutions
     {
         static void Main(string[] args)
         {
+
+            Tree();
             //Console.WriteLine("Is a Palindrome"); 
             //Palindrome pal = new Palindrome();
             //string palindromCharacter = Console.ReadLine();
@@ -22,18 +26,46 @@ namespace InterviewSolutions
             //    Console.Read();  
             //}
 
-            OneAway oAway = new OneAway();
+            //OneAway oAway = new OneAway();
             //if (oAway.oneEditAway("pale", "pae"))
             //    Console.Write("Both Strings are one edit away");
             //else
             //    Console.Write("Both Strings are Not one Edit Away");
 
-            if (oAway.oneEditAwayVersion1("pale", "ble"))
+            //if (oAway.oneEditAwayVersion1("pale", "ble"))
+            //{
+            //    Console.Write("Both Strings are one edit away");
+            //}
+            //else
+            //    Console.Write("Both Strings are Not one Edit Away");
+
+
+
+        }
+
+
+        private static void Tree()
+        {
+            int[] binaryTree = new int[] { 5, 6, 11, 4, 7 };
+            Node root = null;
+            // inserting value;
+            Console.WriteLine("Building binary search Tree");
+            foreach (int value in binaryTree)
             {
-                Console.Write("Both Strings are one edit away");
+                if (root == null)
+                {
+                    Console.WriteLine("Initializing the root node with value: {0}", value.ToString());
+                    root = new Node(value);
+                }
+                else
+                {
+                    Console.WriteLine("Inserting Node {0}", value.ToString());
+                    root = root.Insert(ref root, value);
+                }
             }
-            else
-                Console.Write("Both Strings are Not one Edit Away");
+            Console.WriteLine("Enter value to be deleted");
+            int valueToBeDeleted = Convert.ToInt16(Console.ReadLine());
+            root = root.Delete(ref root, valueToBeDeleted);
 
         }
     }
---
+++ InterviewSolutions/Tree/Tree.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewSolutions.Tree
+{
+    public class Node
+    {
+        private int _value;
+        public int value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public Node(int value)
+        {
+            _value = value;
+        }
+
+        public Node()
+        { }
+
+        public Node LeftNode;
+        public Node RightNode;
+
+
+        /// <summary>
+        /// Insert a value to binary search tree
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Node Insert(ref Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node(value);
+            }
+            else if (root.value == value)
+            {
+                root.value = value;
+            }
+            else if (value < root.value)
+            {
+                Insert(ref root.LeftNode, value);
+            }
+            else
+            {
+                Insert(ref root.RightNode, value);
+            }
+            return root;
+        }
+
+        /// <summary>
+        /// Deletes the value from binary serach tree
+        /// </summary>
+        /// <param name="root">Root node</param>
+        /// <param name="value">Value to be inserted</param>
+        /// <returns>Root Node</returns>
+        public Node Delete(ref Node root, int value)
+        {
+
+            if (root == null)
+                return root;
+            Node _nodeTobeDeleted = root;
+            Node parentNode = null;
+            while (_nodeTobeDeleted.value!=value)
+            {
+                // if the value is greater than the value, it is in left side of the tree
+                if (value > _nodeTobeDeleted.value)
+                {
+                    _nodeTobeDeleted = _nodeTobeDeleted.RightNode;
+                    parentNode = _nodeTobeDeleted;
+                }
+                else if (value < _nodeTobeDeleted.value)
+                {
+                    _nodeTobeDeleted = _nodeTobeDeleted.LeftNode;
+                    parentNode = _nodeTobeDeleted;
+                }
+
+            }
+            // We  now need to "rethread"  the tree.
+            if (_nodeTobeDeleted != null && _nodeTobeDeleted.RightNode == null)
+            {
+                _nodeTobeDeleted = _nodeTobeDeleted.LeftNode;
+                }
+            return root;
+        }
+
+        public int FindMinValue(Node root)
+        {
+            Node currentNode = root;
+            int minValue = 0;
+            while (currentNode.LeftNode != null)
+            {
+                currentNode = currentNode.LeftNode;
+                minValue = currentNode.value;
+            }
+            return minValue;
+
+        }
+
+        public void ReplaceNodeInParent(ref Node root, int newValue =0)
+        {
+
+        }
+
+    }
+}
