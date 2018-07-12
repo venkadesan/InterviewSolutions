d0854eaa2339e2e0f8f714a60dd6eb063d82b285
Venkadesan Vanchimuthu <vshyam.muthu@hotmail.com>
2018-07-11 21:56:05 -0600

    Salesforce programming test questions

--- InterviewSolutions/Program.cs
+++ InterviewSolutions/Program.cs
@@ -13,7 +13,16 @@ namespace InterviewSolutions
         static void Main(string[] args)
         {
 
-            Tree();
+
+            int[] a = new int[] { 1, 2, 4, 3, 4, 5 };
+            int[] rotate = new int[] { 0, 2, 3 };
+            int[] results = getMaxElementIndices(a, rotate);
+
+            //int maxPosition = 10;
+            //int[] start = new int[] { 3, 8 };
+            //int[] end = new int[] { 4, 9 };
+            //int result = widestGap(maxPosition, start, end);
+            //Tree();
             //Console.WriteLine("Is a Palindrome"); 
             //Palindrome pal = new Palindrome();
             //string palindromCharacter = Console.ReadLine();
@@ -65,8 +74,88 @@ namespace InterviewSolutions
             }
             Console.WriteLine("Enter value to be deleted");
             int valueToBeDeleted = Convert.ToInt16(Console.ReadLine());
-            root = root.Delete(ref root, valueToBeDeleted);
+            //root = root.Delete(ref root, valueToBeDeleted);
+
+        }
+
+        private static int[] getMaxElementIndices(int[] a, int[] rotate)
+        {
+            List<int> rotatedResults = new List<int>();
+            try
+            {
+                for (int rotateLength = 0; rotateLength < rotate.Length; rotateLength++)
+                {
+
+                    Queue<int> q = new Queue<int>(a);
+                    Stack<int> s;
+                    int rotateValue = rotate[rotateLength];
+                    while (rotateValue > 0)
+                    {
+                        s = new Stack<int>(q);
+                        int x = s.Pop();
+                        s = new Stack<int>(s);
+                        s.Push(x);
+                        q = new Queue<int>(s);
+                        rotateValue--;
+
+                    }
+                    var result = q.ToArray();
+                    int maxValue = result.Max();
+                    int maxIndex = result.ToList().IndexOf(maxValue);
+                    rotatedResults.Add(maxIndex);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rotatedResults.ToArray();
+        }
+
+                
+        private static int widestGap(int n, int[] start, int[] end)
+        {
+            int gap = 0;
+            if (n > 0 && start.Length > 0 && end.Length > 0 && start.Length == end.Length)
+            {
+                List<int> road = new List<int>(n);
+                int totalNoOfCars = start.Length;
+                List<int> lstofGaps = new List<int>();
+                Queue<int> startQ = new Queue<int>(start);
+                Queue<int> endQ = new Queue<int>(end);
+
+                int startPosition = startQ.Dequeue();
+                int endPosition = endQ.Dequeue();
+                for (int position = 1; position <= road.Capacity; position++)
+                {
+                    if (endPosition > position || position > endPosition)
+                    {
+                        //car startPosition
+                        if (startPosition != position && endPosition != position)
+                        {
+                            gap++;
+                        }
+                    }
+                    else
+                    {
+                        lstofGaps.Add(gap);
+                        gap = 0;
+                        if (startQ.Count > 0)
+                        {
+                            startPosition = startQ.Dequeue();
+                            endPosition = endQ.Dequeue();
+                        }
+                    }
+                }
+
+
+                int maxValue = lstofGaps.ToArray().Max();
+            }
+            return gap;
 
         }
     }
+
+
+
 }
