163f61c41e8721346e4af8e5fead595e86c02d6f
Venkadesan Vanchimuthu <vshyam.muthu@hotmail.com>
2019-07-24 23:03:44 -0500

    Revert "Quick Sorting Algorithm"
    This reverts commit 463ca0880d32344e01ad25ca62281f4339f2bfab.

--- InterviewSolutions/Program.cs
+++ InterviewSolutions/Program.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InterviewSolutions.Tree;
-using InterviewSolutions.Algorithms;
-using InterviewSolutions.Algorithms.Sorting;
 
 
 namespace InterviewSolutions
@@ -15,15 +13,10 @@ namespace InterviewSolutions
         static void Main(string[] args)
         {
 
-            QuickSort qs = new QuickSort();
-            int[] a = new int[] {5,1,7,2,6,4,8,3 };
 
-            int[] result= qs.SortArray(a, 0, a.Length - 1);
-            //int[] a = new int[] { 1, 2, 4, 3, 4, 5 };
-            //int[] rotate = new int[] { 0, 2, 3 };
-            //int[] results = getMaxElementIndices(a, rotate);
-
-
+            int[] a = new int[] { 1, 2, 4, 3, 4, 5 };
+            int[] rotate = new int[] { 0, 2, 3 };
+            int[] results = getMaxElementIndices(a, rotate);
 
             //int maxPosition = 10;
             //int[] start = new int[] { 3, 8 };
