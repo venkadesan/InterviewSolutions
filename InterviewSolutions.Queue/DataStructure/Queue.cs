2926b7f35358b4746350e3eac2a37fbdd571bdf9
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-06-04 13:16:07 -0500

    Queue Implementation

---
+++ InterviewSolutions.Queue/DataStructure/Queue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace InterviewSolutions.Queue.DataStructure
+{
+    #region Queue
+    public class Queue<T>
+    {
+        #region private variables
+        private QueueNode<T> first;
+        private QueueNode<T> last;
+        #endregion
+
+        #region constructor
+        public Queue()
+        {
+        }
+        #endregion
+
+        #region EnQueue
+        public bool EnQueue(T item)
+        {
+            if (item == null)
+            {
+                throw new Exception("item is null");
+            }
+            QueueNode<T> t = new QueueNode<T>(item);
+            if (last != null)
+            {
+                last.next = t;
+
+            }
+            last = t;
+            if (first == null)
+            {
+                first = last;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region DeQueue
+        public T DeQueue()
+        {
+            if (first == null) throw new Exception("There is no item to Queue");
+            T data = first.data;
+            first = first.next;
+            if (first == null)
+            {
+                last = null;
+            }
+            return data;
+
+        }
+        #endregion
+
+        #region Peek
+        public T Peek()
+        {
+            if (first == null)
+            {
+                throw new Exception("Queue is empty");
+            }
+            return first.data;
+        }
+        #endregion
+
+        #region IsEmtpy
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        #endregion
+
+    }
+    #endregion
+
+    #region QueueNode
+    public class QueueNode<T>
+    {
+        public T data;
+        public QueueNode<T> next { get; set; }
+        public QueueNode(T data)
+        {
+            this.data = data;
+        }
+    }
+    #endregion
+
+}
--- InterviewSolutions.Queue/Program.cs
+++ InterviewSolutions.Queue/Program.cs
@@ -6,7 +6,17 @@ namespace InterviewSolutions.Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataStructure.Queue<int> queue = new DataStructure.Queue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Queueing: {i} ");
+                queue.EnQueue(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Dequeuing count : {queue.DeQueue().ToString()}"); 
+            }
         }
     }
 }
