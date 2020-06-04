7d1042b761cb88589f64bbdb40b58bd8fe916ddd
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-06-04 14:45:43 -0500

    Implementing queue using 2 stacks problem

---
+++ InterviewSolutions.Queue/Problem/MyQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolutions.Queue.Problem
+{
+    public class MyQueue<T>
+    {
+        #region private variable
+        private Stack<T> newStack;
+        private Stack<T> oldStack;
+        #endregion
+
+        #region constructor
+        public MyQueue()
+        {
+            newStack = new Stack<T>();
+            oldStack = new Stack<T>();
+        }
+        #endregion
+
+        #region EnQueue
+        /// <summary>
+        ///  Queues item T to queue
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool EnQueue(T item)
+        {
+            newStack.Push(item);
+            return true;
+        }
+        #endregion
+
+        #region DeQueue
+        /// <summary>
+        /// Dequeue the element from queue.
+        /// </summary>
+        /// <returns></returns>
+        public T DeQueue()
+        {
+            shiftStacks();
+            return oldStack.Pop();
+        }
+        #endregion
+
+        #region Peek
+        /// <summary>
+        /// Peeking at queue
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            shiftStacks();
+            return oldStack.Peek();
+        }
+        #endregion
+
+        #region shiftStacks
+        /// <summary>
+        ///  Reversing from new stack to old stack so that we can pop/ peek the elements.
+        /// </summary>
+        private void shiftStacks()
+        {
+            if (oldStack.Count == 0)
+            {
+                while (newStack.Count != 0)
+                {
+                    oldStack.Push(newStack.Pop());
+                }
+            }
+        }
+        #endregion
+
+    }
+}
--- InterviewSolutions.Queue/Program.cs
+++ InterviewSolutions.Queue/Program.cs
@@ -1,11 +1,39 @@
 ﻿using System;
-
+using InterviewSolutions.Queue.Problem;
 namespace InterviewSolutions.Queue
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Custom queue implementation.
+            CustomQueue();
+            // Implemeting Queue using 2 stack data structures.
+            MyQueue();
+
+
+
+        }
+
+        private static void MyQueue()
+        {
+            MyQueue<int> myQueue = new MyQueue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Queueing: {i} ");
+                myQueue.EnQueue(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Dequeuing count : {myQueue.DeQueue().ToString()}");
+            }
+        }
+
+
+        private static void CustomQueue()
+        {
+            // Custom Queue implementation
             DataStructure.Queue<int> queue = new DataStructure.Queue<int>();
             for (int i = 0; i < 10; i++)
             {
@@ -15,7 +43,7 @@ namespace InterviewSolutions.Queue
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Dequeuing count : {queue.DeQueue().ToString()}"); 
+                Console.WriteLine($"Dequeuing count : {queue.DeQueue().ToString()}");
             }
         }
     }
