4c9e628c37cbee9249fa2175e2eb88a43b6655a7
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-06-19 14:51:42 -0500

    Robot grid  problem

---
+++ InterviewSolutions.DP/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolutions.DP
+{
+    public class Grid
+    {
+        public List<point> getPath(bool[][] maze)
+        {
+            return null;
+        }
+    }
+
+    public class point
+    {
+        public int row { get; set; }
+        public int col { get; set; }
+        public point(int _row, int _col)
+        {
+            row = _row;
+            col = _col;
+        }
+    }
+}
