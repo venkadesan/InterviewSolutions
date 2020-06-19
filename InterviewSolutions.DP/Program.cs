0da3f8061db9f6f4ceede2eba5eecbb37e827e4f
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-06-19 15:27:18 -0500

    Robot grid probelm input using 2 * 2 matrix

--- InterviewSolutions.DP/Program.cs
+++ InterviewSolutions.DP/Program.cs
@@ -6,9 +6,31 @@ namespace InterviewSolutions.DP
     {
         static void Main(string[] args)
         {
+
+            // Triple step
+            //TripleStep();
+            // Robot Grid problem
+            RobotGrid();
+
+        }
+
+
+        private static void TripleStep()
+        {
             //1.Triple steps
             Console.WriteLine($"{TripleSteps.CountWays(5)}");
-            
+        }
+
+        private static void RobotGrid()
+        {
+
+            bool[][] matrix = new bool[][]{
+                                            new bool[] { true, true,false},
+                                            new bool[] { true, true,true},
+                                            new bool[] { true, false, true }
+            };
+            Grid grid = new Grid();
+            var path = grid.getPath(matrix);
         }
     }
 }
