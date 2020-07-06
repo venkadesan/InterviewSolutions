24a1cd3f650016b91ff47b3aae0bd255e9199c00
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-06 15:04:02 -0500

    sorted matrix Search algorithm

--- InterviewSolutions.Algorithms/Algorithms/Search/SortedMatrixSearch.cs
+++ InterviewSolutions.Algorithms/Algorithms/Search/SortedMatrixSearch.cs
@@ -6,6 +6,29 @@ namespace InterviewSolutions.Algorithms.Search
 {
     public class SortedMatrixSearch
     {
+        //Solution1 :Naive Solution.
+
+        public bool findElement(int[,] matrix, int elem)
+        {
+            int row = 0;
+            int col = matrix.GetLength(0) - 1;
+            while (row < matrix.Length && col >= 0)
+            {
+                if (matrix[row, col] == elem)
+                {
+                    return true;
+                }
+                else if (matrix[row, col] > elem)
+                {
+                    col--;
+                }
+                else
+                {
+                    row++;
+                }
+            }
+            return false;
+        }
 
     }
 }
