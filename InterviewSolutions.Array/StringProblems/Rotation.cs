883489ff0baa3bb3e21a59fa4102105a5385d43c
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-08 17:18:09 -0500

    String rotation problem

---
+++ InterviewSolutions.Array/StringProblems/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolutions.Array.StringProblems
+{
+    public class Rotation
+    {
+        public bool isRotation(string s1, string s2)
+        {
+            int len = s1.Length;
+            if (len == s2.Length && len > 0)
+            {
+            }
+
+            return false;
+        }
+    }
+}
