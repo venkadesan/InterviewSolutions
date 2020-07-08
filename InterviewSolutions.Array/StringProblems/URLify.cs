d65649778560ef69eade8d3b1626eee59fc49bab
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-08 16:37:57 -0500

    Urlify replacing the spaces

---
+++ InterviewSolutions.Array/StringProblems/URLify.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolutions.Array.StringProblems
+{
+    public class URLify
+    {
+        public void replaceSpaces(char[] str, int length)
+        {
+            int spaceCounter = 0;
+            int index = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    spaceCounter++;
+                }
+            }
+
+            index = length + spaceCounter * 2;
+            if (length < str.Length) str[length] = '\0';
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (str[i] == ' ')
+                {
+                    str[index - 1] = '0';
+                    str[index - 2] = '2';
+                    str[index - 3] = '%';
+                    index = index - 3;
+                }
+                else
+                {
+                    str[index - 1] = str[i];
+                    index--;
+                }
+            }
+        }
+    }
+}
