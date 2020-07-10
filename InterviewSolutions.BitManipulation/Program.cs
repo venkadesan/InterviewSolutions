72acb1341b66c00fdde8ad3b683b4fffa5b72d29
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-10 16:19:25 -0500

    Added projects for Bit Manipulation and Math Logic puzzles

---
+++ InterviewSolutions.BitManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterviewSolutions.BitManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
