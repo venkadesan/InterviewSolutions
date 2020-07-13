9b9bb46bdd7da7dc48382ae7f83e2d50b83f2156
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-13 14:03:57 -0500

    Smallest Difference solution in O( A log A + B log B)

--- InterviewSolution.Medium/Problems/SmallestDifference.cs
+++ InterviewSolution.Medium/Problems/SmallestDifference.cs
@@ -6,6 +6,31 @@ namespace InterviewSolution.Medium.Problems
 {
     public class SmallestDifference
     {
-
+        public int findSmallestDifference(int[] array1, int[] array2)
+        {
+            // Sorting arrays
+            Array.Sort(array1);
+            Array.Sort(array2);
+            int diff = int.MaxValue;
+            int a = 0;
+            int b = 0;
+            while (a < array1.Length && b < array2.Length)
+            {
+                if (Math.Abs(array1[a] - array2[b]) < diff)
+                {
+                    diff = Math.Abs(array1[a] - array2[b]);
+                }
+                // Move smaller value.
+                if (array1[a] > array2[b])
+                {
+                    a++;
+                }
+                else
+                {
+                    b++;
+                }
+            }
+            return diff;
+        }
     }
 }
