2c94c5840f65872c53d367eb5b55bbef2c43b915
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-13 13:31:27 -0500

    Factorial Zeroes problem

--- InterviewSolution.Medium/Problems/FactorialZeros.cs
+++ InterviewSolution.Medium/Problems/FactorialZeros.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace InterviewSolution.Medium.Problems
 {
+    /// <summary>
+    ///  This class counts # of trailing zeros in the in n Factorial.
+    /// </summary>
     public class FactorialZeros
     {
+
+        public int countFactZeroes(int num)
+        {
+            int count = 0;
+            if (num < 0)
+            {
+                return -1;
+            }
+            for (int i = 5; num / i>0; i *= 5)
+            {
+                count += num / i;
+            }
+
+            return count;
+        }
     }
 }
