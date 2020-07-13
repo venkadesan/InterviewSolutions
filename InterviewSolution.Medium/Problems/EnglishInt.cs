d77b8dc6ae2afb53953d62fa23e86a266530e6ab
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-13 16:48:33 -0500

    converting integer to it's equivalent english format

--- InterviewSolution.Medium/Problems/EnglishInt.cs
+++ InterviewSolution.Medium/Problems/EnglishInt.cs
@@ -4,12 +4,75 @@ using System.Text;
 
 namespace InterviewSolution.Medium.Problems
 {
-   public class EnglishInt
+    public class EnglishInt
     {
         string[] smalls = new string[] { "Zero","One", "Two","Three", "Four", "Five","Six","Seven",
                                           "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-        string[] tens = new string[] { "", "", "Twenty", "Thirty", "Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
+        string[] tens = new string[] { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        string[] bigs = new string[] { "", "Thousand", "Million", "Billion" };
 
+        string hundred = "Hundred";
+        string negative = "Negative";
+
+        public string Convert(int num)
+        {
+            if (num == 0)
+            {
+                return smalls[0];
+            }
+            else if (num < 0)
+            {
+                return negative + "" + Convert(-1 * num);
+            }
+
+            LinkedList<string> parts = new LinkedList<string>();
+            while (num > 0)
+            {
+                if (num % 1000 != 0)
+                {
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private string convertChunk(int number)
+        {
+            LinkedList<string> parts = new LinkedList<string>();
+            if (number >= 100)
+            {
+                parts.AddFirst(smalls[number / 100]);
+                parts.AddFirst(hundred);
+                number %= 100;
+            }
+
+            // Converting tens places
+            if (number >= 10 && number <= 19)
+            {
+                parts.AddLast(smalls[number]);
+            }
+            else if (number >= 20)
+            {
+                parts.AddLast(tens[number / 10]);
+                number %= 10;
+            }
+
+            return listToString(parts);
+        }
+
+        private string listToString(LinkedList<string> parts)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (parts.Count > 1)
+            {
+                sb.Append(parts.First);
+
+                sb.Append(" ");
+            }
+            sb.Append(parts.First);
+            parts.RemoveFirst();
+            return sb.ToString();
+        }
 
     }
 }
