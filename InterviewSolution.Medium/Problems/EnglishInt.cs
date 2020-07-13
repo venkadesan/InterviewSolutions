4a0e7b31782e2b33540f713390d5b9ffc5e2894a
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-13 16:35:08 -0500

    Converting integration in English phrase

---
+++ InterviewSolution.Medium/Problems/EnglishInt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolution.Medium.Problems
+{
+   public class EnglishInt
+    {
+        string[] smalls = new string[] { "Zero","One", "Two","Three", "Four", "Five","Six","Seven",
+                                          "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+        string[] tens = new string[] { "", "", "Twenty", "Thirty", "Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
+
+
+    }
+}
