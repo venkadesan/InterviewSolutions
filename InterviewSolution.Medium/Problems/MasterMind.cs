2ae5e1105d9e355a6871bcfce44c8fe221b6e1bf
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-30 16:54:26 -0500

    MasterMind  problem solution

--- InterviewSolution.Medium/Problems/MasterMind.cs
+++ InterviewSolution.Medium/Problems/MasterMind.cs
@@ -7,8 +7,75 @@ namespace InterviewSolution.Medium.Problems
     public class MasterMind
     {
 
+        #region private variables
+        int MAX_COLORS = 4;
+        #endregion
+
+
+        #region public methods
+        public Result estimate(string guess, string solution)
+        {
+            var res = new Result();
+            if (guess.Length != solution.Length)
+            {
+                return null;
+            }
+
+            int[] frequencies = new int[MAX_COLORS];
+            var guessChars = guess.ToCharArray();
+            var solutionChars = solution.ToCharArray();
+
+            for (int i = 0; i < guess.Length; i++)
+            {
+                if (guessChars[i].Equals(solutionChars[i]))
+                {
+                    res.hits++;
+                }
+                else
+                {
+                    int code = Code(solutionChars[i]);
+                    frequencies[code]++;
+                }
+            }
+
+            // Pseduo Code hits
+            for (int i = 0; i < guess.Length; i++)
+            {
+                int code = Code(guess[i]);
+                if (code >= 0 && frequencies[code] > 0 && !guessChars[i].Equals(solutionChars[i]))
+                {
+                    res.pseduoHits++;
+                    frequencies[code]--;
+                }
+            }
+
+            return res;
+        }
+        #endregion
+
+        #region private methods
+        private int Code(char c)
+        {
+            switch (c)
+            {
+                case 'B':
+                    return 0;
+                case 'G':
+                    return 1;
+                case 'R':
+                    return 2;
+                case 'Y':
+                    return 3;
+                default:
+                    return -1;
+            }
+        }
+        #endregion
+
+
     }
 
+    #region Result Class
     public class Result
     {
         public int hits = 0;
@@ -16,8 +83,11 @@ namespace InterviewSolution.Medium.Problems
 
         public override string ToString()
         {
-            return $"";
-
+            return $"{hits}, {pseduoHits}";
         }
+
+
     }
+    #endregion
+
 }
