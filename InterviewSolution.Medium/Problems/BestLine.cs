364808917da646760196dcbb0ded7146daa065a3
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-30 11:11:14 -0500

    Best Line problem

--- InterviewSolution.Medium/Problems/BestLine.cs
+++ InterviewSolution.Medium/Problems/BestLine.cs
@@ -7,19 +7,36 @@ namespace InterviewSolution.Medium.Problems
     public class BestLine
     {
 
+        #region public methods
         public Line FindBestLine(GraphPoint[] points)
         {
             var lineByScope = GetListOfLines(points);
             return GetBestLine(lineByScope);
         }
+        #endregion
 
-        public Dictionary<double, List<Line>> GetListOfLines(GraphPoint[] points)
+        #region private methods
+
+        private Dictionary<double, List<Line>> GetListOfLines(GraphPoint[] points)
         {
             var linesBySlope = new Dictionary<double, List<Line>>();
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    Line line = new Line(points[i], points[j]);
+                    double key = Line.floorToNearestEpislon(line.slope);
+                    if (!linesBySlope.ContainsKey(key))
+                    {
+                        linesBySlope.Add(key, new List<Line>());
+                    }
+                    linesBySlope[key].Add(line);
+                }
+            }
             return linesBySlope;
         }
 
-        public Line GetBestLine(Dictionary<double, List<Line>> linesBySlope)
+        private Line GetBestLine(Dictionary<double, List<Line>> linesBySlope)
         {
             Line bestLine = null;
             int bestCount = 0;
@@ -27,13 +44,48 @@ namespace InterviewSolution.Medium.Problems
             foreach (double slope in slopes)
             {
                 List<Line> lines = linesBySlope[slope];
+                foreach (var line in lines)
+                {
+                    int count = CountEquivalentLines(linesBySlope, line);
+                    if (count > bestCount)
+                    {
+                        bestLine = line;
+                        bestCount = count;
+                        //bestLine.Print();
+                        Console.WriteLine($" best count: {bestCount}");
+                    }
 
+                }
             }
             return null;
         }
 
+        private int CountEquivalentLines(Dictionary<double, List<Line>> linesBySlope, Line line)
+        {
+            double key = Line.floorToNearestEpislon(line.slope);
+            int count = CountEquivalentLines(linesBySlope[key], line);
+            count += CountEquivalentLines(linesBySlope[key - Line.epsilon], line);
+            count += CountEquivalentLines(linesBySlope[key + Line.epsilon], line);
+            return count;
+        }
 
+        private int CountEquivalentLines(List<Line> lines, Line line)
+        {
+            if (lines == null) return 0;
+            int count = 0;
+            foreach (Line pLine in lines)
+            {
+                if (pLine.IsEquivalent(line))
+                {
+                    count++;
+                }
+            }
+            return count;
 
+        }
+        #endregion
+
+        #region Line Class
         public class Line
         {
             public static double epsilon = .0001;
@@ -75,13 +127,23 @@ namespace InterviewSolution.Medium.Problems
                 return (Math.Abs(a - b) < epsilon);
             }
         }
+        #endregion
+
+        #region Graph point
 
         public class GraphPoint
         {
             public double X { get; set; }
             public double Y { get; set; }
-        }
-    }
 
+            public GraphPoint(double _x, double _y)
+            {
+                X = _x;
+                Y = _y;
+            }
+        }
+        #endregion
+
+    }
 
 }
