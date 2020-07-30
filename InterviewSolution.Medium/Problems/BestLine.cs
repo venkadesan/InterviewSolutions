242e0e6b7479d48325ec7827d8e565bd261cdcd9
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-07-30 10:13:58 -0500

    Best Line by Graph points solution

--- InterviewSolution.Medium/Problems/BestLine.cs
+++ InterviewSolution.Medium/Problems/BestLine.cs
@@ -7,5 +7,40 @@ namespace InterviewSolution.Medium.Problems
     public class BestLine
     {
 
+        public Line FindBestLine(GraphPoint[] points)
+        {
+            Dictionary<double, Line> lineByScope = GetListOfLines(points);
+
+            return GetBestLine(lineByScope);
+        }
+
+        public Dictionary<double, Line> GetListOfLines(GraphPoint[] points)
+        {
+            return null; 
+        }
+
+        public Line GetBestLine(Dictionary<double, Line> linesBySlope)
+        {
+            return null; 
+        }
+
+
+
+        public class Line
+        {
+            public static double epsilon = .0001;
+            public double slope, intercept;
+            private bool infinite_slope = false;
+
+            //public Line(Graph)
+        }
+
+        public class GraphPoint
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
     }
+
+
 }
