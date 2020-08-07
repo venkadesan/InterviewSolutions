0303f257c673583d2e652aaf751a121ddd0a8166
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-06 20:34:11 -0500

    Sub Sort implementation

--- InterviewSolution.Medium/Problems/SubSort.cs
+++ InterviewSolution.Medium/Problems/SubSort.cs
@@ -7,5 +7,35 @@ namespace InterviewSolution.Medium.Problems
     public class SubSort
     {
 
+        #region public methods
+        public void findUnsortedSequence(int[] array)
+        {
+
+        }
+        #endregion
+
+
+        #region private methods
+        private int findEndOfLeftSequence(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < array[i + 1]) return i + 1;
+            }
+            return 0;
+        }
+
+        private int findStartOfRightSequence(int[] array)
+        {
+            for (int i = array.Length - 2; i >= 0; i++)
+            {
+                if (array[i] > array[i + 1]) return i + 1;
+            }
+            return 0;
+        }
+        #endregion
+
+
+
     }
 }
