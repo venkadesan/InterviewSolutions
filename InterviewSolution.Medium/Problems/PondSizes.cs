67f2ecd21a0d21c9c89c8fa0174935665089e643
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-12 22:46:42 -0500

    PondSizes solution

--- InterviewSolution.Medium/Problems/PondSizes.cs
+++ InterviewSolution.Medium/Problems/PondSizes.cs
@@ -6,6 +6,48 @@ namespace InterviewSolution.Medium.Problems
 {
     public class PondSizes
     {
+        #region public methods
+        public List<int> ComputePondSizes(int[,] land)
+        {
+            var sizes = new List<int>();
+            if (land != null && land.Length > 0)
+            {
+                bool[,] visited = new bool[land.GetLength(0), land.GetLength(1)];
+                for (int r = 0; r < land.GetLength(0); r++)
+                {
+                    for (int c = 0; c < land.GetLength(1); c++)
+                    {
+                        int size = computeSize(land, visited, r, c);
+                        sizes.Add(size);
+                    }
+                }
+
+            }
+            return sizes;
+        }
+        #endregion
+
+        #region private methods
+        private int computeSize(int[,] land, bool[,] visited, int row, int col)
+        {
+            int size = 1;
+
+            if (row < 0 || col < 0 || row >= land.GetLength(0) || col >= land.GetLength(row) || visited[row, col] || land[row, col] != 0)
+            {
+                return 0;
+            }
+            visited[row, col] = true;
+            for (int dr = -1; dr <= 1; dr++)
+            {
+                for (int dc = -1; dc <= 1; dc++)
+                {
+                    size += computeSize(land, visited, row + dr, col + dr);
+                }
+            }
+            return size;
+        }
+        #endregion
+
 
     }
 }
