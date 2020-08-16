fe56f5a8c67797d8b4d2ad99e81a7549f2a8b0c0
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-15 22:52:17 -0500

    Javascript return statement difference

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -9,17 +9,41 @@ console.log("b defined?" + (typeof b !== 'undefined'));
 var bar = null;
 console.log(typeof bar != null && typeof bar === "object");
 
+
 var myobject = {
     foo: "bar",
-    func = (function(){
+    func: function() {
         var self = this;
         console.log("outer func: this.foo =" + this.foo);
         console.log("outer func: self.foo =" + self.foo);
         (function () {
-            console.log("inner func:this.foo" + this.foo);
-            console.log("inner func:self.foo" + self.foo);
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
         }());
+
     }
 };
 
 myobject.func();
+
+function foo1() {
+    return {
+        bar: "hello"
+    };
+}
+
+function foo2() {
+    return
+    {
+        bar: "hello"
+    };
+}
+
+
+console.log("foo1 returns:");
+console.log(foo1());
+console.log("foo2 returns:");
+console.log(foo2());
+
+
+
