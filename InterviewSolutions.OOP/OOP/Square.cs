655048c6ae677e79f0e96b3c7f7e57671fb1f7ba
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-17 22:02:08 -0500

    Pattern Matching problem

--- InterviewSolutions.OOP/OOP/Square.cs
+++ InterviewSolutions.OOP/OOP/Square.cs
@@ -2,8 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace InterviewSolutions.OOP.OOP
+namespace InterviewSolutions.OOP
 {
+
+    public static class Area
+    {
+        public static double ComputeAreaModernIs(object shape)
+        {
+            if (shape is Square s)
+            {
+                return s.Side * s.Side;
+            }
+            else if (shape is Circle c)
+            {
+                return Math.PI * c.Radius * c.Radius;
+            }
+            else if (shape is Trianage T)
+            {
+                return (T.Base * T.Height) / 2;
+            }
+            else if (shape is Rectange R)
+            {
+                return R.Height * R.Length;
+            }
+            throw new ArgumentException(message: "Shape is not a recognized shape", paramName: nameof(shape));
+        }
+    }
+
     public class Square
     {
         public double Side { get; set; }
@@ -40,7 +65,7 @@ namespace InterviewSolutions.OOP.OOP
     {
         public double Base { get; set; }
         public double Height { get; set; }
-        public Trianage(double @base,double height)
+        public Trianage(double @base, double height)
         {
             Base = @base;
             Height = @height;
--- InterviewSolutions.OOP/Program.cs
+++ InterviewSolutions.OOP/Program.cs
@@ -6,7 +6,21 @@ namespace InterviewSolutions.OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Pattern Matching problem.
+            PatternMatching();
+        }
+
+
+        private static void PatternMatching()
+        {
+            var square = new Square(5.0);
+            var triangle = new Trianage(1.0, 2.0);
+            var rectange = new Rectange(1.0, 5.0);
+            var circle = new Circle(2.0);
+            Console.WriteLine($"SQUARE AREA :  { Area.ComputeAreaModernIs(square)}");
+            Console.WriteLine($"TRIANGLE AREA : { Area.ComputeAreaModernIs(triangle)}");
+            Console.WriteLine($"RECTANGLE AREA :  { Area.ComputeAreaModernIs(rectange)}");
+            Console.WriteLine($"CIRCEL AREA: { Area.ComputeAreaModernIs(circle)}");
         }
     }
 }
