cee2b878cbf62e340b7f3f37c1d9cd0c3d403b22
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-19 09:38:26 -0500

    JS features

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -12,7 +12,7 @@ console.log(typeof bar != null && typeof bar === "object");
 
 var myobject = {
     foo: "bar",
-    func: function() {
+    func: function () {
         var self = this;
         console.log("outer func: this.foo =" + this.foo);
         console.log("outer func: self.foo =" + self.foo);
@@ -46,6 +46,7 @@ console.log("foo2 returns:");
 console.log(foo2());
 
 
+/* Finds The lowest common multiple of two numbers  */
 function LCMCalculator() {
     let checkInt = function (x) {
         if (x % 1 !== 0) {
@@ -54,17 +55,58 @@ function LCMCalculator() {
         this.a = checkInt(x)
         this.b = checkInt(y);
     }
+
+
 }
 
+/*    */
 LCMCalculator.prototype = {
     constructor: LCMCalculator,
     gcd: function () {
         let a = Math.abs(this.a), b = Math.abs(this.b), t;
         if (a < b) {
-            [a,b] =[]
+            [a, b] = [b, a];
+        }
+        while (b !== 0) {
+            t = b;
+            b = a % b;
+            a = t;
         }
     }
 }
 
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 == 0.3);
+
+
+function AreTheNumbersEqual(num1, num2) {
+    return Math.abs(num1 - num2) < Number.EPSILON;
+}
+
+console.log(AreTheNumbersEqual(0.1 + 0.1, 0.2));
+
+(function () {
+    console.log(1);
+    setTimeout(function () { console.log(2) }, 1000);
+    setTimeout(function () { console.log(3) }, 0);
+    console.log(4);
+})();
+
+function isPalindrome(str) {
+    str = str.replace(/\W/g, '').toLowerCase();
+    return (str == str.split('').reverse().join(''));
+}
+
+var paliText = 'liril';
+console.log('palindrom:' + isPalindrome(paliText));
+
+function sum(x) {
+    if (arguments.length == 2) {
+        return arguments[0] + arguments[1];
+    }
+    else {
+        return function (y) { return x + y; };
+    }
+}
 
 
