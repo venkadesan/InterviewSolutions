5cbd33300f995af8d10b2a0f557cea954cb75e5d
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-08-26 23:16:42 -0500

    RandomSet problem - solution

---
+++ InterviewSolution.Hard/Problems/RandomSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewSolution.Hard.Problems
+{
+    public class RandomSet
+    {
+        public int[] PickMInIteratively(int[] original, int m)
+        {
+            int[] subset = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                subset[i] = original[i];
+            }
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                int k = Rand(0, i);
+                if (k < m)
+                {
+                    subset[k] = original[i];
+                }
+            }
+            return subset;
+        }
+
+        private int Rand(int lower, int higher)
+        {
+            return lower + (int)(new Random().Next(0, 1) * (higher - lower) + 1);
+        }
+    }
+}
