a3be12bc3b1f55f8433bd2d085f9c2d95a59b8b7
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-09-02 10:05:23 -0500

    Circus Tower Solution

--- InterviewSolution.Hard/Problems/CircusTower.cs
+++ InterviewSolution.Hard/Problems/CircusTower.cs
@@ -6,6 +6,42 @@ namespace InterviewSolution.Hard.Problems
 {
     public class CircusTower
     {
+        public List<HeightWeight> GetLongestSequeuence(List<HeightWeight> heightWeights)
+        {
+            var longestWeight = new List<HeightWeight>();
 
+            return longestWeight;
+        }
     }
+
+    #region HeightWeight
+
+    public class HeightWeight : IComparable<HeightWeight>
+    {
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+
+        #region constructor
+        public HeightWeight(decimal height, decimal weight)
+        {
+            Height = height;
+            Weight = weight;
+        }
+        #endregion
+
+
+        public int CompareTo(HeightWeight other)
+        {
+            if (Height != other.Height)
+            {
+                return Height.CompareTo(other.Height);
+            }
+            else
+            {
+                return Weight.CompareTo(other.Weight);
+            }
+        }
+    }
+    #endregion
+
 }
