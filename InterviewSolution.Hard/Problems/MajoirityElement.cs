fef7c92b5a6e4979d91e7bb0aa0ba1f5ec2a000d
Venkadesan Vanchimuthu <vshyam.muthu@gmail.com>
2020-09-03 12:55:21 -0500

    FindMajorityElement -optimal solution

--- InterviewSolution.Hard/Problems/MajoirityElement.cs
+++ InterviewSolution.Hard/Problems/MajoirityElement.cs
@@ -6,10 +6,61 @@ namespace InterviewSolution.Hard.Problems
 {
     public class MajoirityElement
     {
-        public int FindMajorityElement()
+
+        #region public methods
+
+        #region FindMajorityElement
+        public int FindMajorityElement(List<int> numbers)
         {
-            //int 
+            if (numbers != null && numbers.Count > 0)
+            {
+                int candidate = GetCandidate(numbers);
+                return validate(numbers, candidate) ? candidate : -1;
+            }
             return 0;
         }
+        #endregion
+        #endregion
+
+
+        #region private methods
+        private int GetCandidate(List<int> numbers)
+        {
+            int majority = 0;
+            if (numbers != null && numbers.Count > 0)
+            {
+                int count = 0;
+                foreach (var num in numbers)
+                {
+                    if (count == 0)
+                    {
+                        majority = count;
+                    }
+                    if (num == majority)
+                    {
+                        count++;
+                    }
+                    else { count--; }
+                }
+
+            }
+            return majority;
+        }
+
+
+        private bool validate(List<int> numbers, int majority)
+        {
+            int count = 0;
+            foreach (var num in numbers)
+            {
+                if (num == majority)
+                {
+                    count++;
+                }
+            }
+            return count > (numbers.Count / 2);
+        }
+        #endregion
+
     }
 }
